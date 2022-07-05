250a638cddc1a0c803a0229231aec42b1b9f36dd
iskander <iuljaev@gmail.com>
2022-07-05 15:43:27 +0500

    Added new Header section

--- src/components/NavBar/NavBar.vue
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line vue/multi-word-component-names
-<template>
-  <div>This is for navbar component.</div>
-</template>
--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
-    This is a home page and home view. We are currentky on dev branch.
+    <HeaderSection />
   </div>
 </template>
 
 <script>
+import HeaderSection from "@/components/HeaderSection.vue/HeaderSection.vue";
 export default {
   name: "HomeView",
+  components: { HeaderSection },
 };
 </script>
 
