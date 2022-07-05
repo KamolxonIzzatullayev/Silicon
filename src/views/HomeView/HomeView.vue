9588f86e1cf02d845d656e5d88a7633f51254e06
iskander <iuljaev@gmail.com>
2022-07-05 10:27:29 +0500

    Done by Kamolxon

---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">This is a home page and home view</div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./HomeView.style.scss";
+</style>
