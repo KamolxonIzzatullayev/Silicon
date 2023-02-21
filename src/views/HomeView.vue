d7ade1337bf966781a22782d25f75e998c767bcd
ustabayevaxadxon <ustabayevkhan@gmail.com>
2023-02-21 13:59:05 +0500

    Added tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+    <div class="text-2xl font-bold text-blue-800">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni,
+        accusamus.
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+    name: "HomeView",
+    components: {
+        HelloWorld,
+    },
+};
 </script>
